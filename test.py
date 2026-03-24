33130f4095c584eebbc55318836ff8a4e03b21ea
3XM <101142491+Melyguy@users.noreply.github.com>
2026-03-24 09:21:45 +0100

    good progress

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import cv2
+from mediapipe import mp
 
 cap = cv2.VideoCapture(0)
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+mp_draw = mp.solutions.drawing_utils
 
 if not cap.isOpened():
     print("Cannot open camera")
@@ -10,6 +15,14 @@ while True:
     if not ret: break
 
     cv2.imshow('Webcam', frame)
+
+    result = hands.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+    if result.multi_hand_landmarks:
+        for hand_landmarks in result.multi_hand_landmarks:
+            mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+    cv2.imshow('Hand Tracking', frame)
 
     if cv2.waitKey(1) == ord('q'):
         break
